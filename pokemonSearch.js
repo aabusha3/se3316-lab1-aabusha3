4fa8d9018bb023bca6b47bf67d3ae6f1bf22f84a
aabusha3 <aabusha3@uwo.ca>
2022-09-13 23:31:35 -0400

    add get methods for name & num search and a print

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -15,4 +15,22 @@ class pokeInfoGen{
         this.fastMoves = fastMoves;
         this.chargeMoves = chargeMoves;
     }
+
+    get pName(){
+        return this.name;
+    }
+
+    get pNum(){
+        return this.num;
+    }
+
+	get print(){
+        return "" + this.name + ":\nThis Pokemon is Num #" + this.num + " From Gen #" + this.gen + " And Has The Type: " + this.type
+        + "\nIts Evovlution Stage Is At #" + this.evolveLvl + " And Needs " + this.candy + " Candies To Evolve"
+        + "\nWith " + (this.parent=="None" ? "No " : this.parent + " As The ") + "Previous Evolution, And " + (this.descendant=="None" ? "No " : this.descendant + " As The ") 
+        + "Next Evolution\nIt Is Found Easiest In " + this.weather + " Weather"
+        + "\n\nATK: " + this.atk + " DEF: " + this.def + " STA: " + this.sta 
+        + "\nFast Moves: " + this.fastMoves 
+        + "\nCharge Moves: " + this.chargeMoves + "\n\n";
+    }   
 }
