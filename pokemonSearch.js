035437f366bef104e21229f177717af88967345e
aabusha3 <aabusha3@uwo.ca>
2022-09-13 23:30:27 -0400

    add class and constructor to store pokemon info

---
+++ pokemonSearch.js
@@ -0,0 +1,18 @@
+class pokeInfoGen{
+    constructor(name="", num="", gen=0, parent="", descendant="", candy=0, evolveLvl=0, type="", weather="", atk=0, def=0, sta=0, fastMoves="",chargeMoves="") {
+        this.name = name;    
+        this.num = num;      
+        this.gen = gen;
+        this.parent = parent; 
+        this.descendant = descendant; 
+        this.candy = candy; 
+        this.evolveLvl = evolveLvl; 
+        this.type = type; 
+        this.weather = weather; 
+        this.atk = atk; 
+        this.def = def; 
+        this.sta = sta; 
+        this.fastMoves = fastMoves;
+        this.chargeMoves = chargeMoves;
+    }
+}
