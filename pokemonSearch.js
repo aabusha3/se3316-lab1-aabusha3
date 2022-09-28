ef3282a16c3e058dc49ab3b1e18a11f6b50b1ad1
aabusha3 <aabusha3@uwo.ca>
2022-09-28 15:28:40 -0400

    Add the specific image urls to the updated pokeArray

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -31,32 +31,32 @@ class pokeInfoGen{
 
 function pokeArray(){
    
-    const bulbasaur = new pokeInfoGen("Bulbasaur", "001", "Grass / Poison",1,1);
-    const ivysaur = new pokeInfoGen("Ivysaur", "002", "Grass / Poison",1,2);
-    const venusaur = new pokeInfoGen("Venusaur", "003", "Grass / Poison",1,3);
+    const bulbasaur = new pokeInfoGen("Bulbasaur", "001", "Grass / Poison", 1,1, "images/1.png");
+    const ivysaur = new pokeInfoGen("Ivysaur", "002", "Grass / Poison", 1,2, "images/2.png");
+    const venusaur = new pokeInfoGen("Venusaur", "003", "Grass / Poison", 1,3, "images/3.png");
     
-    const charmander = new pokeInfoGen("Charmander", "004", "Fire",1,1);
-    const charmeleon = new pokeInfoGen("Charmeleon", "005", "Fire",1,2);
-    const charizard = new pokeInfoGen("Charizard", "006", "Fire / Flying",1,3);
+    const charmander = new pokeInfoGen("Charmander", "004", "Fire", 1,1, "images/4.png");
+    const charmeleon = new pokeInfoGen("Charmeleon", "005", "Fire", 1,2, "images/5.png");
+    const charizard = new pokeInfoGen("Charizard", "006", "Fire / Flying", 1,3, "images/6.png");
     
-    const squirtle = new pokeInfoGen("Squirtle", "007", "Water",1,1);
-    const wartortle = new pokeInfoGen("Wartortle", "008", "Water",1,2);
-    const blastoise = new pokeInfoGen("Blastoise", "009", "Water",1,3);
+    const squirtle = new pokeInfoGen("Squirtle", "007", "Water", 1,1, "images/7.png");
+    const wartortle = new pokeInfoGen("Wartortle", "008", "Water", 1,2, "images/8.png");
+    const blastoise = new pokeInfoGen("Blastoise", "009", "Water", 1,3, "images/9.png");
     
-    const caterpie = new pokeInfoGen("Caterpie", "010", "Bug",1,1);
-    const metapod = new pokeInfoGen("Metapod", "011", "Bug",1,2);
-    const butterfree = new pokeInfoGen("Butterfree", "012", "Bug / Flying",1,3);
+    const caterpie = new pokeInfoGen("Caterpie", "010", "Bug", 1,1, "images/10.png");
+    const metapod = new pokeInfoGen("Metapod", "011", "Bug", 1,2, "images/11.png");
+    const butterfree = new pokeInfoGen("Butterfree", "012", "Bug / Flying", 1,3, "images/12.png");
     
-    const weedle = new pokeInfoGen("Weedle", "013", "Bug / Poison",1,1);
-    const kakuna = new pokeInfoGen("Kakuna", "014", "Bug / Poison",1,2);
-    const beedrill = new pokeInfoGen("Beedrill", "015", "Bug / Poison",1,3);
+    const weedle = new pokeInfoGen("Weedle", "013", "Bug / Poison", 1,1, "images/13.png");
+    const kakuna = new pokeInfoGen("Kakuna", "014", "Bug / Poison", 1,2, "images/14.png");
+    const beedrill = new pokeInfoGen("Beedrill", "015", "Bug / Poison", 1,3, "images/15.png");
     
-    const pidgey = new pokeInfoGen("Pidgey", "016", "Normal / Flying",1,1);
-    const pidgeotto = new pokeInfoGen("Pidgeotto", "017", "Normal / Flying",1,2);
-    const pidgeot = new pokeInfoGen("Pidgeot", "018", "Normal / Flying",1,3);
+    const pidgey = new pokeInfoGen("Pidgey", "016", "Normal / Flying", 1,1, "images/16.png");
+    const pidgeotto = new pokeInfoGen("Pidgeotto", "017", "Normal / Flying", 1,2, "images/17.png");
+    const pidgeot = new pokeInfoGen("Pidgeot", "018", "Normal / Flying", 1,3, "images/18.png");
     
-    const rattata = new pokeInfoGen("Rattata", "019", "Normal",1,1);
-    const raticate = new pokeInfoGen("Raticate", "020", "Normal",1,2);
+    const rattata = new pokeInfoGen("Rattata", "019", "Normal", 1,1, "images/19.png");
+    const raticate = new pokeInfoGen("Raticate", "020", "Normal", 1,2, "images/20.png");
     
     return [bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, squirtle, wartortle, blastoise, caterpie, metapod, butterfree, 
         weedle, kakuna, beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate];
